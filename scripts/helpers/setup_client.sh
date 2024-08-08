3c2c38b5979c29a7cbcab6f6f0efcbe7a5e74076
NoureddineMaher <nmaher@rubicon.ma>
2024-08-08 12:13:00 +0100

    fix: 'update the client_setup'

--- scripts/helpers/setup_client.sh
+++ scripts/helpers/setup_client.sh
@@ -4,13 +4,13 @@ PROJECT_ROOT="$(dirname "$(realpath "$0")")/../.."
 CLIENT_DIR="$PROJECT_ROOT/client"
 CLIENT_PACKAGES_FILE="$PROJECT_ROOT/scripts/dependencies/clientRequirements.txt"
 
-echo "Starting client setup..."
+echo "Starting client setup...⏳"
 if [ -d "$CLIENT_DIR" ]; then
     echo "Navigating to $CLIENT_DIR..."
     cd "$CLIENT_DIR" || exit
 
     if [ ! -f "package.json" ]; then
-        echo "package.json not found. Running npm init -y..."
+        echo "package.json not found. Running npm init -y...🧹🧹"
         npm init -y
     fi
 
@@ -21,17 +21,17 @@ if [ -d "$CLIENT_DIR" ]; then
         echo "Found package file at $CLIENT_PACKAGES_FILE. Installing packages..."
         while IFS= read -r package; do
             if [ ! -z "$package" ]; then
-                echo "Installing $package..."
+                echo "Installing $package...⏳⏳⏳"
                 npm install "$package"
             fi
         done < "$CLIENT_PACKAGES_FILE"
     else
-        echo "No package file found at $CLIENT_PACKAGES_FILE."
+        echo "🔴 🔴  No package file found at $CLIENT_PACKAGES_FILE."
     fi
 
     cd - || exit
 else
-    echo "Directory $CLIENT_DIR does not exist."
+    echo "🔴 Directory $CLIENT_DIR does not exist."
 fi
 
-echo "Client setup complete ! Happy Coding :)"
+echo "Client setup complete ! Happy Coding ✅✅✅"
